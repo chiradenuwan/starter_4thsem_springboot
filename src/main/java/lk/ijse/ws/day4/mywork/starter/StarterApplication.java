259fefc3c0d8a98ace9c7a3f759cbbcf0ea24897
chira <chiranjayadenuwan@gmail.com>
2020-10-31 15:36:15 +0530

    Initial Commit in project

---
+++ src/main/java/lk/ijse/ws/day4/mywork/starter/StarterApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.ws.day4.mywork.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterApplication.class, args);
+	}
+
+}
