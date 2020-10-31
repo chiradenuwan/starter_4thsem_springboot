4f07c156a295b8aee1e1d7fb369d01c1fd3dac15
chira <chiranjayadenuwan@gmail.com>
2020-10-31 16:52:18 +0530

    Create a Get method with Requestparams

--- src/main/java/lk/ijse/ws/day4/mywork/starter/api/MyRestController.java
+++ src/main/java/lk/ijse/ws/day4/mywork/starter/api/MyRestController.java
@@ -2,6 +2,7 @@ package lk.ijse.ws.day4.mywork.starter.api;
 
 import lk.ijse.ws.day4.mywork.starter.data.Message;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
@@ -15,6 +16,18 @@ public class MyRestController {
         Message message = new Message();
         message.setSender("Sever");
         message.setMessage("Hello World");
+        message.setSendTime(new Date());
+        return message;
+
+    }
+
+    @GetMapping("/api/message")
+    public Message messageBuilder(
+            @RequestParam("username")
+                    String user, @RequestParam("message") String msg) {
+        Message message = new Message();
+        message.setSender(user);
+        message.setMessage(msg);
         message.setSendTime(new Date());
         return message;
 
