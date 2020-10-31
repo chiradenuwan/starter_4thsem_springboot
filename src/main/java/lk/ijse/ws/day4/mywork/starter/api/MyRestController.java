698f8874f47a33ed999e6469645cac69a643ebdc
chira <chiranjayadenuwan@gmail.com>
2020-10-31 16:12:16 +0530

    set Message Object in greeting() Get Mapping

--- src/main/java/lk/ijse/ws/day4/mywork/starter/api/MyRestController.java
+++ src/main/java/lk/ijse/ws/day4/mywork/starter/api/MyRestController.java
@@ -5,19 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
-import java.util.concurrent.atomic.AtomicLong;
 
-/*
-@RestController
-@RequestMapping("/api")
-public class MyRestController {
-
-    @GetMapping(path = "/hello", produces = "text/plain")
-    public ResponseEntity<Message> sayHello() {
-        Message message = new Message();
-        return new ResponseEntity<Message>(message, HttpStatus.OK);
-    }
-}*/
 @RestController
 public class MyRestController {
 
