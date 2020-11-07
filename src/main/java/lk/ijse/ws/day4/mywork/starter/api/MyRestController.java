ed4a32c364ac0b5c8989a447f6f778fd286cff86
chira <chiranjayadenuwan@gmail.com>
2020-11-07 14:12:48 +0530

    create message table in database

--- src/main/java/lk/ijse/ws/day4/mywork/starter/api/MyRestController.java
+++ src/main/java/lk/ijse/ws/day4/mywork/starter/api/MyRestController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Date;
 
 @RestController
+
 public class MyRestController {
 
 
--- src/main/java/lk/ijse/ws/day4/mywork/starter/data/Message.java
+++ src/main/java/lk/ijse/ws/day4/mywork/starter/data/Message.java
@@ -1,8 +1,16 @@
 package lk.ijse.ws.day4.mywork.starter.data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Date;
 
+@Entity
 public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
     private String sender;
     private String message;
     private Date sendTime;
@@ -14,6 +22,14 @@ public class Message {
         this.sender = sender;
         this.message = message;
         this.sendTime = sendTime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getSender() {
